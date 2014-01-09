277df86aff90d32269f31ac979a423dcb5256bbe
Chris Antenesse <chris@foundapp.com>
2014-01-09 13:29:13 -0800

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'elasticservices'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures elasticservices'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
